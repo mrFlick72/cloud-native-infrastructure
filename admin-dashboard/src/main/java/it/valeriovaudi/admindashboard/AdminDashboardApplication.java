47a5609f4916280929b330ddbd8bd452f9622311
mrFlick72 <valerio.vaudi@gmail.com>
2019-08-21 00:28:50 +0200

    spring boot admin logout issue

--- admin-dashboard/src/main/java/it/valeriovaudi/admindashboard/AdminDashboardApplication.java
+++ admin-dashboard/src/main/java/it/valeriovaudi/admindashboard/AdminDashboardApplication.java
@@ -3,6 +3,9 @@ package it.valeriovaudi.admindashboard;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.config.annotation.SecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,4 +19,13 @@ public class AdminDashboardApplication {
         SpringApplication.run(AdminDashboardApplication.class, args);
     }
 
+}
+
+@EnableWebSecurity
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().oauth2Login();
+    }
 }
